e4616038e8137f2c8df266d7081e9a45d50f91b6
David Riha <kontakt@rihadavid.cz>
2024-10-16 15:20:09 +0800

    Update route.ts

--- app/api/lambda-chat/route.ts
+++ app/api/lambda-chat/route.ts
@@ -41,7 +41,8 @@ async function* streamResponse(wsUrl: string, message: string, custom_session_id
   while (true) {
     const chunk = await new Promise<string>((resolve) => {
       ws.onmessage = (event) => {
-          console.log('webscocket received message: ' + JSON.stringify(event));
+          console.log('webscocket received message: ' + event);
+          console.log('webscocket received message event.data: ' + event.data);
         resolve(event.data);
       };
     });
